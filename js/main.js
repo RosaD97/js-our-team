f36fef439853380ddb5d1367d32aaf4e67efc5e5
RosaD97 <r960595@gmail.com>
2023-03-07 15:18:55 +0100

    MILESTONE 0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// Tutte le informazioni dei membri del team
+const info = [
+    {
+       nome : 'Wayne Barnett',
+       ruolo : 'Founder & CEO',	
+       foto : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome : 'Angela Caroll',
+        ruolo : 'Chief Editor',	
+        foto : 'wangela-caroll-chief-editor.jpg'
+     },
+     {
+        nome : 'Walter Gordon',
+        ruolo : 'Office Manager',	
+        foto : 'walter-gordon-office-manager.jpg'
+     },
+     {
+        nome : 'Angela Lopez',
+        ruolo : 'Social Media Manager',	
+        foto : 'angela-lopez-social-media-manager.jpg'
+     },
+     {
+         nome : 'Scott Estrada',
+         ruolo : 'Developer',	
+         foto : 'scott-estrada-developer.jpg'
+      },
+      {
+         nome : 'Barbara Ramos',
+         ruolo : 'Graphic Designer',	
+         foto : 'barbara-ramos-graphic-designer.jpg'
+      }
+]
